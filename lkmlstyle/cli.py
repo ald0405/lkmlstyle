a7468574be7ee68ca59f4d7cfe16618fd0875b06
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2022-02-15 14:42:03 -0500

    Validate files or entire directories

--- lkmlstyle/cli.py
+++ lkmlstyle/cli.py
@@ -8,45 +8,71 @@ from lkmlstyle.check import check
 def main():
     console = Console(stderr=True)
     parser = argparse.ArgumentParser(description="A flexible style checker for LookML.")
-    parser.add_argument("file", type=pathlib.Path, help="path to the file to check")
+    parser.add_argument(
+        "path",
+        nargs="+",
+        type=pathlib.Path,
+        help="path(s) to the file or directory to check",
+    )
     parser.add_argument(
         "--ignore",
         nargs="+",
         metavar="CODE",
         required=False,
         help="rule codes to exclude from checking, like 'D106' or 'M200'",
+        default=[],
+    )
+    parser.add_argument(
+        "--select",
+        nargs="+",
+        metavar="CODE",
+        required=False,
+        help="only check the specified rule codes, like 'D106' or 'M200'",
+        default=[],
     )
     args = parser.parse_args()
 
-    with args.file.open("r") as file:
-        text = file.read()
-
-    violations = check(text, ignore=args.ignore)
-
-    lines = text.split("\n")
+    paths = []
+    for path in args.path:
+        if path.is_dir():
+            paths.extend(path.glob("**/*.lkml"))
+        else:
+            paths.append(path)
 
     console.print()
-    console.rule(
-        f"Found {len(violations)} LookML style issues",
-        style="#9999ff",
-    )
-    console.print()
-    for violation in violations:
-        code, title, line_number = violation
-        console.print(f"{code} [bold red]{title}[/bold red]")
-        console.print(f"{args.file}:{line_number}")
-        console.rule(style="grey30")
+    for path in sorted(set(paths)):
+        violations = []
+        with path.open("r") as file:
+            text = file.read()
+        violations.extend(
+            check(text, select=tuple(args.select), ignore=tuple(args.ignore))
+        )
+        lines = text.split("\n")
 
-        for n in range(line_number - 1, line_number + 3):
-            if n <= 0:
-                continue
-            if n == line_number:
-                console.print(f"{n:<4} >| {escape(lines[n - 1])}", highlight=False)
-            else:
-                console.print(
-                    f"[dim]{n:<4}[/dim]  | [dim]{lines[n - 1]}[/dim]", highlight=False
-                )
+        console.rule(str(path), style="#9999ff")
         console.print()
+        for violation in violations:
+            code, title, line_number = violation
+            console.print(f"{code} [bold red]{title}[/bold red]")
+            console.print(f"{path}:{line_number}")
+            console.rule(style="grey30")
+
+            for i, n in enumerate(range(line_number - 1, line_number + 3)):
+                if n <= 0:
+                    continue
+                # Don't print whitespace-only leading lines
+                elif i == 0 and lines[n - 1].strip() == "":
+                    continue
+                elif n == line_number:
+                    console.print(
+                        f" {n:<4} [red]>[/red]| {escape(lines[n - 1])}", highlight=False
+                    )
+                else:
+                    console.print(
+                        f" [dim]{n:<4}[/dim]  | [dim]{lines[n - 1]}[/dim]",
+                        highlight=False,
+                    )
+            console.print()
 
 
 if __name__ == "__main__":
